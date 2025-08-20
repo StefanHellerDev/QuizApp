a97f5937273a249b89692c768596ff534ac5c164
Stefan Heller <S_Heller@gmx.de>
2025-08-20 15:35:19 +0200

    added question and answers in JSON

---
+++ script.js
@@ -0,0 +1,51 @@
+let questions = [
+    {
+        "question" : "Welches HTML-Tag wird verwendet, um ein externes JavaScript einzubinden?",
+        "answer_1" : "<javascript>",
+        "answer_2" : "<js>",
+        "answer_3" : "<script>",
+        "answer_4" : "<link>",
+        "rightAnswer" : 3
+    },
+    {
+        "question" : "Welches CSS-Attribut wird verwendet, um den Abstand außerhalb eines Elements festzulegen?",
+        "answer_1" : "padding",
+        "answer_2" : "spacing",
+        "answer_3" : "margin",
+        "answer_4" : "border",
+        "rightAnswer" : 3
+    },
+    {
+        "question" : "Welche HTTP-Methode wird im Frontend typischerweise für das Abrufen von Daten verwendet?",
+        "answer_1" : "GET",
+        "answer_2" : "POST",
+        "answer_3" : "PUT",
+        "answer_4" : "DELETE",
+        "rightAnswer" : 1
+    },
+    {
+        "question" : "Was macht die CSS-Eigenschaft display: flex;?",
+        "answer_1" : "Richtet Texte zentriert aus",
+        "answer_2" : "Aktiviert ein Layout-Modell zur flexiblen Anordnung von Elementen",
+        "answer_3" : "Verhindert Umbrüche von Text",
+        "answer_4" : "Blendet ein Element aus",
+        "rightAnswer" : 2
+    },
+    {
+        "question" : "Welche JavaScript-Methode wird genutzt, um ein Element anhand seiner ID auszuwählen?",
+        "answer_1" : "document.querySelector()",
+        "answer_2" : "document.getElementsByClassName()",
+        "answer_3" : "document.getElementById()",
+        "answer_4" : "document.getElementByTag()",
+        "rightAnswer" : 3
+    },
+    {
+        "question" : "Welche Dateiendung hat eine React-Komponente üblicherweise?",
+        "answer_1" : ".jsx",
+        "answer_2" : ".html",
+        "answer_3" : ".rc",
+        "answer_4" : ".ts",
+        "rightAnswer" : 1
+    }
+];
+
